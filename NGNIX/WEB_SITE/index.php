53a353592f7087c0a035b42cb53edf1ba1b27b5c
Theo PAYEN <tt.payen@gmail.com>
2022-10-16 20:52:21 +0200

    update

---
+++ NGNIX/WEB_SITE/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <title>TEST docker</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+        <script src='main.js'></script>
+    </head>
+    <body>
+        <h1>Docker load balancing</h1>
+        <h2>Hostname:<?php echo gethostname();?></h2>
+        <img src="img/logo.png">
+    </body>
+</html>
